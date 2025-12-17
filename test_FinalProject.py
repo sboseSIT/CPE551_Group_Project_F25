8ec58db809bf840c395f92b7f9a1f1dd5cdce6f8
sboseSIT <sbose5@stevens.edu>
2025-12-17 17:41:22 -0500

    Add files via upload
    Created three tests: 1 checks if the correct timeframe is assigned based on timeofday data. The other two check if the data columns of temp, humidity, and energy usage are all numbers (one passes and one fails for two separate cases to show functionality). These are housed in a test_.py file.

---
+++ test_FinalProject.py
@@ -0,0 +1,78 @@
+import pytest
+import pandas as pd
+
+from Bose_Giardino_buildingClass import Building
+from Bose_Giardino_buildingCalcClass import BuildingCalc
+
+@pytest.fixture
+def fake_data():
+    return pd.DataFrame({
+        "Timestamp": [
+            "2025-01-01 07:00:00",  # morning example
+            "2025-01-01 13:00:00",  # afternoon example
+            "2025-01-01 19:00:00",  # evening example
+        ],
+        "Energy_Usage (kWh)": [10, 20, 30],
+        "Temperature (C)": [12, 24, 36],
+        "Humidity (%)": [40, 45, 50],
+        "Occupancy_Level": ["Low", "Medium", "High"],
+        "Building_Type": ["Industrial", "Commercial", "Residential"]
+    })
+
+@pytest.fixture
+def fail_fake_data():
+    return pd.DataFrame({
+    "Timestamp": [
+            "2025-01-01 07:00:00",
+            "2025-01-01 13:00:00",
+            "2025-01-01 19:00:00",
+        ],
+        #has different cases of invalid data
+        "Energy_Usage (kWh)": [10, "NA", 30],
+        "Temperature (C)": ["cold", 24, 36], 
+        "Humidity (%)": [40, None, 50], 
+        "Occupancy_Level": ["Low", "Medium", "High"],
+        "Building_Type": ["Industrial", "Commercial", "Residential"]
+    })
+
+def test_timeofdaycategory(fake_data):
+    #Ensure BuildingCalc Class creates a new column and creates timestep categories.
+    #Must utilize both classes to test this functionality
+    a = Building(fake_data)
+    b = BuildingCalc(a)
+
+    #Using assert to test for the timeofday categorization.
+    assert "TimeOfDay" in b.df.columns
+    assert b.df.loc[0, "TimeOfDay"] == "morning"
+    assert b.df.loc[1, "TimeOfDay"] == "afternoon"
+    assert b.df.loc[2, "TimeOfDay"] == "evening"
+
+def test_numeric_columns_valid(fake_data):
+    #Check all expected number columns are numeric WITH VALID DATASET (expecting to work).
+    a = Building(fake_data)
+    b = BuildingCalc(a)
+
+    numeric_columns = [
+        "Temperature (C)",
+        "Temperature (F)",
+        "Humidity (%)",
+        "Energy_Usage (kWh)"]
+
+    for col in numeric_columns:
+        converted = pd.to_numeric(b.df[col], errors="coerce")
+        assert converted.notna().all(), f"Non-numeric values found in {col}"
+
+def test_numeric_columns_invalid(fail_fake_data):
+    #Check all expected number columns are numeric WITH VALID DATASET (expecting to fail).
+    a = Building(fail_fake_data)
+    b = BuildingCalc(a)
+
+    numeric_columns = [
+        "Temperature (C)",
+        "Temperature (F)",
+        "Humidity (%)",
+        "Energy_Usage (kWh)"]
+
+    for col in numeric_columns:
+        converted = pd.to_numeric(b.df[col], errors="coerce")
+        assert converted.notna().all(), f"Non-numeric values found in {col}"
