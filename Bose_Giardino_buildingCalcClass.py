0a6ed19bb56090fb54cf1e62cf4a2dc80ab34660
nicolelynne13 <ngiardin@stevens.edu>
2025-12-17 16:19:55 -0500

    Add files via upload
    Moved the Building class into Bose_Giardino_buildingClass.py and the BuildingCalc class into Bose_Giardino_buildingCalcClass.py for the requirement, "Projects should separate code into meaningful files and modules". I updated the main ipynb script to account for this and to import these files. Also completed some minor updates across the entire code, including adding new titles in for the graphs (getting it ready for submission).

---
+++ Bose_Giardino_buildingCalcClass.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class BuildingCalc:
+    """
+    This class has a composition relationship with the Building class. 
+    Ex. Week 12 Composition Slides: "has-a" relationship
+    Class function is to calculate estimates for energy, temp, humidity based
+    on inputs of: building type, occupancy level, and time
+    """
+    def __init__(self, building):
+        #this is a constructor.
+        #Storing the building instance in this separate class.
+        self.building = building #composition within this class to the previous class
+        # dataframe from the building instance.
+        df = self.building.df.copy()
+        #Filtering the data to create a new timeofday column (separates timestamps)
+        df["TimeOfDay"] = df["Timestamp"].apply(self._categorize_time)
+        # getting the data in Fahrenheit rather than Celcius 
+        df["Temperature (F)"] = [temp * 9/5 + 32 for temp in df["Temperature (C)"]] # list comprehension
+        self.df = df
+        
+
+    def _categorize_time(self, ts):
+        #This function works by sorting the timestamps into morning, afternoon, evening, night
+        hour = pd.to_datetime(ts).hour
+
+        if 6 <= hour < 12:
+            return "morning"
+        elif 12 <= hour < 17:
+            return "afternoon"
+        elif 17 <= hour < 23:
+            return "evening"
+        else:
+            return "night"
+        
+    #Determines the avg energy, temp, humidity given the:
+    #Building type, occupancy level, and time period
+    def estimate(self, building_type, occupancy_level, time_period, show_rows = False):
+        #Process details:
+        #Select all rows which fulfill the filter requirements
+        # Use filter + lambda to select matching rows
+        # Source: Week 14 – Built-in functions (filter, lambda)
+        filtered = pd.DataFrame(
+            #Filter is an example of a built in module/functionality
+            filter(
+                lambda row: 
+                (row["Building_Type"] == building_type and row["Occupancy_Level"] == occupancy_level and row["TimeOfDay"] == time_period), self.df.to_dict("records"))
+        )
+
+        #State there is no data for this case, if there is no match- using valueerror
+        if filtered.empty:
+            raise ValueError("No data available for the given parameters.")
+        
+
+        #This if statement is for testing to ensure the correct data rows are used for testing
+        #if show_rows:
+        #    print("Filtered DataFrame used for calculation:")
+        #    print(filtered)
+        #    print()
+
+        #Calc the average of filtered datasets
+        return {
+            "Estimated Temperature (C)": filtered["Temperature (C)"].mean(),
+
+            "Estimated Temperature (F)": filtered["Temperature (F)"].mean(),
+            "Temperature Minimum (F)": filtered["Temperature (F)"].min(),
+            "Temperature Maximum (F)": filtered["Temperature (F)"].max(),
+
+            "Estimated Humidity (%)": filtered["Humidity (%)"].mean(),
+            "Humidity Minimum (%)": filtered["Humidity (%)"].min(),
+            "Humidity Maximum (%)": filtered["Humidity (%)"].max(),
+
+            "Estimated Energy (kWh)": filtered["Energy_Usage (kWh)"].mean(),
+            "Energy Minimum (kWh)": filtered["Energy_Usage (kWh)"].min(),
+            "Energy Maximum (kWh)": filtered["Energy_Usage (kWh)"].max()
+        } 
+    def row_check(self, building_type, occupancy_level, time_period):
+        #Generator function to show rows matching criteria input by user
+        for row in self.df.to_dict("records"):
+            if (
+                row["Building_Type"] == building_type and 
+                row["Occupancy_Level"] == occupancy_level and 
+                row["TimeOfDay"] == time_period
+            ):
+                yield row
---
+++ Bose_Giardino_buildingClass.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Building:
+    def __init__(self, dataframe):
+        # sets up the full dataset rather than just the selected one
+        #PANDAS Dataframe is mutable
+        self.df = dataframe.copy()
+
+        # storing lists of all the data - looks at columns and pulls them as attributes
+        self.timestamps = self.df["Timestamp"]
+        self.energy = pd.to_numeric(self.df["Energy_Usage (kWh)"], errors="coerce")
+        self.temperature = pd.to_numeric(self.df["Temperature (C)"], errors="coerce")
+        self.humidity = pd.to_numeric(self.df["Humidity (%)"], errors="coerce")
+        self.occupancy = self.df["Occupancy_Level"]
+        self.building_type = self.df["Building_Type"]
+        
+    def __len__(self): #operator overloading for len() function
+        return len(self.df)
+
+    def __str__(self): # returns a readable string summarizing dataset for checking, #len() is also a built in module/library
+        return (
+            f"Total records: {len(self)}\n"
+            f"Average Energy Usage: {self.energy.mean():.2f}\n"
+            f"Average Temperature: {self.temperature.mean():.2f}\n"
+            f"Average Humidity: {self.humidity.mean():.2f}"
+        )
